ef2c9bcb9450c9e450d72b8a122e577e58ac0aa6
Denny Hong <hong961127@gmail.com>
2022-01-09 10:51:01 -0800

    update THREE.js boilerplate

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,18 +1,32 @@
+// THREE.js helps add and descirbe data in 3 dimensions (as meshes & lights)
+// then help convert 3 dimensions data into 3d representation onto a <canvas/>
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
+// a scene sets up what is to be rendered by THREE.js and where it is in 3D coords
+// we can then add objects and lightings into a scene
 const scene = new THREE.Scene();
+// scene.background = new THREE.Color(0xff0000);
 
+// a camera describes the view boundaries of the scene
+// perspective camera mimics the way human eye sees
+// closer objects appears larger, furthur objects appears smaller
 const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+  75, // FOV
+  window.innerWidth / window.innerHeight, // aspect ratio
+  0.1, // near clipping plane
+  1000 // far clipping plane
 );
 camera.position.z = 2;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+// a renderer displays the scene onto a HTML canvas element
+const canvas = document.querySelector("#scene1")!;
+const renderer = new THREE.WebGLRenderer({ canvas }); // WebGL allows GPU-accelerated image processing
+renderer.setSize(window.innerWidth, window.innerHeight); // must set a size for WebGL renderer
+// document.body.appendChild(renderer.domElement); // renderer.domElement is the <canvas/> element
+// console.dir(renderer.domElement);
+
+const control = new OrbitControls(camera, renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({
@@ -23,6 +37,8 @@ const material = new THREE.MeshBasicMaterial({
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
+console.dir(scene);
+
 window.addEventListener("resize", onWindowResize, false);
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -32,12 +48,10 @@ function onWindowResize() {
 }
 
 function animate() {
-  requestAnimationFrame(animate);
-
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
-
   render();
+  requestAnimationFrame(animate);
 }
 
 function render() {
