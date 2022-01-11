cc4bef4b41197b90801ecb769266ff65957158c0
Denny Hong <hong961127@gmail.com>
2022-01-10 20:44:46 -0800

    Convert to class syntax

--- src/client/client.ts
+++ src/client/client.ts
@@ -5,87 +5,106 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import Stats from "three/examples/jsm/libs/stats.module";
 import { GUI } from "dat.gui";
 
-// a scene sets up what is to be rendered by THREE.js and where it is in 3D coords
-// we can then add objects and lightings into a scene
-const scene = new THREE.Scene();
-// scene.background = new THREE.Color(0xff0000);
+class App {
+  private scene: THREE.Scene;
+  private camera: THREE.PerspectiveCamera;
+  private renderer: THREE.WebGLRenderer;
 
-// a camera describes the view boundaries of the scene
-// perspective camera mimics the way human eye sees
-// closer objects appears larger, furthur objects appears smaller
-const camera = new THREE.PerspectiveCamera(
-  75, // fov
-  window.innerWidth / window.innerHeight, // aspect ratio
-  0.1, // near clipping plane
-  1000 // far clipping plane
-);
-camera.position.z = 2;
+  private control: OrbitControls;
 
-// a renderer displays the scene onto a HTML canvas element
-const renderer = new THREE.WebGLRenderer(); // WebGL allows GPU-accelerated image processing
-renderer.setSize(window.innerWidth, window.innerHeight); // must set a size for WebGL renderer
-document.body.appendChild(renderer.domElement); // renderer.domElement is the <canvas/> element
-// console.dir(renderer.domElement);
+  private cube: THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial>;
 
-const control = new OrbitControls(camera, renderer.domElement);
-// control.addEventListener("change", render);
-// if OrbitControls doesn't have it's own change handler,
-// it needs render to be called in animate to work
+  private stats: Stats;
+  private gui: GUI;
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00,
-  wireframe: true,
-});
+  constructor() {
+    // a scene sets up what is to be rendered by THREE.js and where it is in 3D coords
+    // we can then add objects and lightings into a scene
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color("#333");
+    console.dir(this.scene);
 
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+    // a camera describes the view boundaries of the scene
+    // perspective camera mimics the way human eye sees
+    // closer objects appears larger, furthur objects appears smaller
+    this.camera = new THREE.PerspectiveCamera(
+      75, // fov
+      window.innerWidth / window.innerHeight, // aspect ratio
+      0.1, // near clipping plane
+      1000 // far clipping plane
+    );
+    this.camera.position.z = 2;
 
-console.dir(scene);
+    // a renderer displays the scene onto a HTML canvas element
+    this.renderer = new THREE.WebGLRenderer(); // WebGL allows GPU-accelerated image processing
+    this.renderer.setSize(window.innerWidth, window.innerHeight); // must set a size for WebGL renderer
+    document.body.appendChild(this.renderer.domElement); // renderer.domElement is the <canvas/> element
+    // console.dir(this.renderer.domElement);
 
-window.addEventListener("resize", onWindowResize, false);
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  render();
+    this.control = new OrbitControls(this.camera, this.renderer.domElement);
+    // this.control.addEventListener("change", render);
+    // if OrbitControls doesn't have it's own change handler,
+    // it needs render to be called in animate to work
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({
+      color: 0x00ff00,
+      wireframe: true,
+    });
+    this.cube = new THREE.Mesh(geometry, material);
+    this.scene.add(this.cube);
+
+    // Stats panel (useful in dev mode)
+    this.stats = Stats();
+    document.body.appendChild(this.stats.domElement);
+
+    // GUI adds a basic UI to interact with the 3d scene and the objects within it.
+    this.gui = new GUI();
+    this.addGui();
+
+    window.addEventListener("resize", this.handleResize.bind(this), false);
+    this.animate();
+  }
+
+  private addGui(): void {
+    const cubeFolder = this.gui.addFolder("Cube"); // for grouping controls
+    cubeFolder.add(this.cube.rotation, "x", 0, Math.PI * 2);
+    cubeFolder.add(this.cube.rotation, "y", 0, Math.PI * 2);
+    cubeFolder.add(this.cube.rotation, "z", 0, Math.PI * 2);
+    cubeFolder.open(); // group is default open
+    const cameraFolder = this.gui.addFolder("Camera");
+    cameraFolder.add(this.camera.position, "z", 0, 20);
+    cameraFolder.open();
+  }
+
+  private handleResize(): void {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.render();
+  }
+
+  private render(): void {
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  // animate() commonly called "the game loop" in game development
+  private animate(): void {
+    // requestAnimationFrame tells the browser to to perform an animation
+    // and requests that the browser invokes the callback to update
+    // an animation before next repaint. callback in invoked about 60times/sec
+    requestAnimationFrame(this.animate.bind(this));
+
+    // stats.begin() && stats.end() measure perf of operations in between
+    // this.stats.begin();
+    this.cube.rotation.x += 0.01;
+    this.cube.rotation.y += 0.01;
+    // this.stats.end();
+
+    this.render();
+
+    this.stats.update();
+  }
 }
 
-// Stats panel (useful in dev mode)
-const stats = Stats();
-document.body.appendChild(stats.domElement);
-
-// GUI adds a basic UI to interact with the 3d scene and the objects within it.
-const gui = new GUI();
-const cubeFolder = gui.addFolder("Cube"); // for grouping controls
-cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2);
-cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2);
-cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2);
-cubeFolder.open(); // group is default open
-const cameraFolder = gui.addFolder("Camera");
-cameraFolder.add(camera.position, "z", 0, 20);
-cameraFolder.open();
-
-function animate() {
-  // requestAnimationFrame tells the browser to to perform an animation
-  // and requests that the browser invokes the callback to update
-  // an animation before next repaint. callback in invoked about 60times/sec
-  requestAnimationFrame(animate);
-
-  // stats.begin() && stats.end() measure perf of operations in between
-  // stats.begin();
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  // stats.end();
-
-  render();
-
-  stats.update();
-}
-
-function render() {
-  renderer.render(scene, camera);
-}
-
-animate(); // or commonly called "the game loop" in game development
-// render();
+new App();
